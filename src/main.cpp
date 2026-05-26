841f29bf7378c0838978723f42037a518bb87dd3
Schlosser1914 <schlosser.s@proton.me>
2026-05-26 19:46:52 +0200

    testing slider with prfintf  to see if works

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,28 @@
 #include "pottislider.h"
 #include <Arduino.h>
 
-
 pottislider slider_rechts(34, 0, 100);
 
+void setup()
+{
 
-
-
-void setup() {
-  
   slider_rechts.begin_pottislider();
-  
-
-}
-
-
-
-
-
-void loop() {
-
-
 
 
 
 
 }
 
+void loop()
+{
+
+int rawwert = slider_rechts.readdater();
+
+Serial.printf("Wert von pottie %4d",rawwert);
+
+
+
+
+delay(100);
+
+}
--- src/pottislider.cpp
+++ src/pottislider.cpp
@@ -1,25 +1,30 @@
 #include "pottislider.h"
 
-pottislider::pottislider(uint8_t pinx,int minvalx, int maxvlx)
-            :pin(pinx),minval(minvalx),maxval(maxvlx){}
+pottislider::pottislider(uint8_t pinx, int minvalx, int maxvlx)
+    : pin(pinx), minval(minvalx), maxval(maxvlx) {}
+
 
 
 void pottislider::begin_pottislider()
 {
-     // ADC-Auflösung auf 12-bit setzen (ESP32-Standard)
+    // ADC-Auflösung auf 12-bit setzen (ESP32-Standard)
     analogReadResolution(12);
 }
 
 
-int pottislider::read_raw(){
 
-return analogRead(pin);
 
+int pottislider::read_raw()
+{
+
+    return analogRead(pin);
 }
 
 
-int pottislider::readdater(){
 
-    return map(read_raw(),0 , 4095, minval ,maxval);
 
+int pottislider::readdater()
+{
+
+    return map(read_raw(), 0, 4095, minval, maxval);
 }
